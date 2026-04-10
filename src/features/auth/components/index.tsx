b294f88edf0a9680f39029dc011ed9350c7c43ec
Mahmoudramadan21 <106267261+Mahmoudramadan21@users.noreply.github.com>
2026-04-10 05:53:57 +0200

    feat: implement reset password feature with form schemas, hooks, and UI components

---
+++ src/features/auth/components/index.tsx
@@ -0,0 +1,16 @@
+/* =========================== Login Form =========================== */
+export { default as LoginForm } from "./login-form";
+
+/* =========================== Register Steps =========================== */
+export { default as EmailStep } from "./email-step";
+export { default as OTPStep } from "./otp-step";
+export { default as UserInfoStep } from "./user-info-step";
+
+/* =========================== Register Progress Steps =========================== */
+export { default as RegisterProgressSteps } from "./progress-steps";
+
+/* =========================== Forgot Password Form =========================== */
+export { default as ForgotPasswordForm } from "./forgot-password-form";
+
+/* =========================== Reset Password Form =========================== */
+export { default as ResetPasswordForm } from "./reset-password-form";
