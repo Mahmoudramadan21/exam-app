15f138525be092c10f1e2feb6685e87baeda478a
Mahmoudramadan21 <106267261+Mahmoudramadan21@users.noreply.github.com>
2026-04-20 00:02:27 +0200

    feat: implement role-based dashboard sidebar and configure SVGR for icons

---
+++ src/features/dashboard/lib/constants/sidebar.constant.ts
@@ -0,0 +1,90 @@
+import {
+  GraduationCap,
+  UserRound,
+  BoltIcon,
+  LogOut,
+  LayoutGrid,
+  BookOpen,
+  FileText,
+  Users,
+  BookOpenCheck,
+  Logs,
+} from "lucide-react";
+
+import { ISidebarLink, SidebarDropdownItem } from "../types/sidebar";
+
+export const USER_SIDEBAR_LINKS: ISidebarLink[] = [
+  {
+    label: "Diplomas",
+    href: "/diplomas",
+    icon: GraduationCap,
+  },
+  {
+    label: "Account Settings",
+    href: "/account",
+    icon: UserRound,
+  },
+];
+
+export const USER_SIDEBAR_DROPDOWN_ITEMS: SidebarDropdownItem[] = [
+  {
+    label: "Account",
+    href: "/account",
+    icon: UserRound,
+    action: "link",
+  },
+  {
+    label: "Dashboard",
+    href: "/dashboard",
+    icon: BoltIcon,
+    action: "link",
+  },
+  {
+    label: "Logout",
+    icon: LogOut,
+    action: "logout",
+  },
+];
+
+export const ADMIN_SIDEBAR_LINKS: ISidebarLink[] = [
+  {
+    label: "Diplomas",
+    href: "/diplomas",
+    icon: GraduationCap,
+  },
+  {
+    label: "Exams",
+    href: "/exams",
+    icon: BookOpenCheck,
+  },
+  {
+    label: "Account Settings",
+    href: "/account",
+    icon: UserRound,
+  },
+  {
+    label: "Audit Log",
+    href: "/audit-log",
+    icon: Logs,
+  },
+];
+
+export const ADMIN_SIDEBAR_DROPDOWN_ITEMS: SidebarDropdownItem[] = [
+  {
+    label: "Account",
+    href: "/account",
+    icon: UserRound,
+    action: "link",
+  },
+  {
+    label: "Application",
+    href: "/dashboard",
+    icon: LayoutGrid,
+    action: "link",
+  },
+  {
+    label: "Logout",
+    icon: LogOut,
+    action: "logout",
+  },
+];
