6e4ead78b966f6811ab4d86c58a9e09ae3707489
Mahmoudramadan21 <mahmoud.fci@gmail.com>
2026-04-27 19:53:40 +0300

    refactor: overhaul authentication system with modular hooks, schema validation, and server actions while migrating utility logic to shared modules.

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,31 +1,43 @@
 "use client";
 
+import { useState } from "react";
+import Link from "next/link";
+
 import ForgotPasswordForm from "@/features/auth/components/forgot-password-form";
 import AuthFormLayout from "@/features/auth/layout/auth-form-layout";
-import { cn } from "@/shared/lib/utils/tailwind-cn";
-import Link from "next/link";
-import { useState } from "react";
+import { forgotPasswordSteps } from "@/features/auth/lib/types/auth";
 
-export type forgotPasswordSteps = "email" | "successSentCode";
+import { cn } from "@/shared/lib/utils/tailwind-cn";
 
 function ForgotPassword() {
+  // ===== Auth flow state (step-based UI) =====
   const [step, setStep] = useState<forgotPasswordSteps>("email");
+
   const [email, setEmail] = useState("");
 
   return (
     <AuthFormLayout title="Forgot Password">
-      <p className="mt-3 mb-7 text-gray-500">
-        Don’t worry, we will help you recover your account.
-      </p>
+      {/* =====================================================
+        STEP 1: Email input
+      ===================================================== */}
       {step === "email" && (
-        <ForgotPasswordForm setStep={setStep} setEmail={setEmail} />
+        <>
+          <p className="mt-3 text-gray-500">
+            Don’t worry, we will help you recover your account.
+          </p>
+
+          <ForgotPasswordForm setStep={setStep} setEmail={setEmail} />
+        </>
       )}
 
+      {/* =====================================================
+        STEP 2: Success message after sending reset email
+      ===================================================== */}
       {step === "successSentCode" && (
-        <div className="mt-6 mb-9 ">
+        <div className="mt-4 mb-9">
           <p className="text-gray-800">
             We have sent a password reset link to:{" "}
-            <span className="text-blue-600">{email}. </span>
+            <span className="text-blue-600">{email}.</span>
           </p>
 
           <p className="my-5 text-gray-800">
@@ -40,6 +52,9 @@ function ForgotPassword() {
         </div>
       )}
 
+      {/* =====================================================
+        Footer link (always visible)
+      ===================================================== */}
       <div
         className={cn(
           "text-sm text-muted-foreground font-medium",
